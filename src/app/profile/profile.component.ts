9541a55c3fd956e0351e5f996fabdecc32d040d8
unknown <andrea.ximena.cs@gmail.com>
2022-12-09 21:39:33 -0500

    profile-navbar-footer

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogBoxComponent } from '../home/dialog-box/dialog-box.component';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { QuestionBoxComponent } from '../home/question-box/question-box.component';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
+  openDialog(){
+    this.dialog.open(DialogBoxComponent,{
+      width:'100%',
+      height:'auto',
+      data: "right click"
+    })
+  }
+  openDialogQuestion(){
+    this.dialog.open(QuestionBoxComponent,{
+      width:'80%',
+      height:'auto',
+      data: "right click"
+    })
+  }
 
   ngOnInit(): void {
   }
+  showText() { //leer más y leer menos texto experiencia
+    this.isReadMore = !this.isReadMore
+ }
+isReadMore = true
+
 
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,6 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
@@ -11,7 +20,16 @@ import { ProfileComponent } from './profile.component';
   ],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatBottomSheetModule,
+    MatListModule,
+    MatSelectModule,
+    ProfileRoutingModule,
+    MatDialogModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class ProfileModule { }
